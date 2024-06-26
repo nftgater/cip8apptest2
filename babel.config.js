b2415d033df94d39973de8036de4f70602ac9095
UnburnedAlliance <linenotsecure@gmail.com>
2024-06-25 23:09:37 -0500

    vite-plugin-top-level-await

---
+++ babel.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    presets: [
+      [
+        '@babel/preset-env',
+        {
+          targets: {
+            node: 'current', // or appropriate target environment
+          },
+        },
+      ],
+    ],
+  };
+  
--- vite.config.ts
+++ vite.config.ts
@@ -5,13 +5,14 @@ import wasm from 'vite-plugin-wasm';
 import vue from '@vitejs/plugin-vue'
 
 //import wasm from './node_modules/vite-plugin-wasm';
-//import topLevelAwait from "vite-plugin-top-level-await";
+import topLevelAwait from "vite-plugin-top-level-await";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    vue(),
     wasm(),
+    topLevelAwait(), 
+    vue(),
   ],
   resolve: {
     alias: {
