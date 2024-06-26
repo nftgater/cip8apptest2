81205fee777c41b99b1f4e1024516468a0534ef8
UnburnedAlliance <linenotsecure@gmail.com>
2024-06-25 22:49:17 -0500

    messed with vite config

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-import wasm from 'vite-plugin-wasm';
+import wasm from './node_modules/vite-plugin-wasm';
 
 // https://vitejs.dev/config/
 export default defineConfig({
