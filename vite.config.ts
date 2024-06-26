a11b1e28ccad91a5b33283c77ffd2e72579e12a1
UnburnedAlliance <linenotsecure@gmail.com>
2024-06-25 22:56:43 -0500

    fuck this fucking fucking fuck fuck

--- vite.config.ts
+++ vite.config.ts
@@ -3,15 +3,14 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-import wasm from './node_modules/vite-plugin-wasm';
-import topLevelAwait from "vite-plugin-top-level-await";
+//import wasm from './node_modules/vite-plugin-wasm';
+//import topLevelAwait from "vite-plugin-top-level-await";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
-    wasm(), 
-    topLevelAwait()
+
   ],
   resolve: {
     alias: {
