806c69be79fd6a834a31254c6b625627dba39f22
UnburnedAlliance <linenotsecure@gmail.com>
2024-06-26 17:21:19 -0500

    prepping comments for sharing

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,11 +1,11 @@
 <template>
   <div class="greetings">
     <h1 class="green">{{ msg }}</h1>
-    <button>Do Shit</button>
+    <button id="login-btn">Do Shit</button>
   </div>
 </template>
 
-<script setup lang="ts">
+<script lang="ts">
 import { Buffer } from "buffer";
 let csl, wallet;
 
@@ -14,11 +14,33 @@ async function loadCsl(){
 };
 loadCsl();
 
-
-
 /*
+// NOTES: 
+// The commented-out functions come from this CIP-0008 guide: https://developers.cardano.org/docs/integrate-cardano/user-wallet-authentication/
+
+// The crash comes from the first usage of the cardano-serialization-library, in getStakeAddress(), after clicking the button to run authenticate().
+// >>> This one >>> ``` const changeAddress = csl.Address.from_bytes( Buffer.from(changeAddrHex, 'hex') ); ```
+
+// As-is, this app runs fine. As soon as I add the functions below 
 
 
+window.addEventListener("load", () => {
+    const loginBtn = document.querySelector("#login-btn");
+    loginBtn.addEventListener("click", authenticate);
+})
+
+async function authenticate(){
+    if (!csl) await loadCsl(); // make sure CSL is loaded before doing anything else.
+
+    wallet = await window.cardano.nami.enable(); // was: typhoncip30
+
+    const [stakeAddrHex, stakeAddrBech32] = await getStakeAddress();
+    const messageUtf = `account: ${stakeAddrBech32}`;
+    const messageHex = Buffer.from(messageUtf).toString("hex");    
+    const sigData = await wallet.signData(stakeAddrHex, messageHex);
+    const result = await submitToBackend(sigData);
+    alert(result.message);
+}
 
 
 async function getStakeAddress(){
@@ -34,6 +56,7 @@ async function getStakeAddress(){
     return [stakeAddress.to_hex(), stakeAddress.to_bech32()];
 }
 
+// Not even worried about this part right now.
 async function submitToBackend(sigData){
     const result = await fetch(`http://localhost:8081/login`, {
         method: "POST",
@@ -45,10 +68,19 @@ async function submitToBackend(sigData){
     return result.json();
 }
 */
+export default {
+	data() {
+		return {
+      csl: null,
+    }
+  },
+  methods: {},
+  mounted() {
 
-defineProps<{
-  msg: string
-}>()
+  }
+}
+
+
 </script>
 
 <style scoped>
