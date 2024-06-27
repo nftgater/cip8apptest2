97ae680a02b48a72aa340b8cb152387b1a9f69c3
UnburnedAlliance <linenotsecure@gmail.com>
2024-06-26 19:09:50 -0500

    refining the about page

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,17 +1,20 @@
 <template>
   <div class="about">
     <h1>Made by <a href="https://twitter.com/_theunburned_" target="_blank">Bluetoast</a></h1>
-    <p>Public <a href="https://github.com/nftgater/cip8apptest2">github repo</a>, check the README for notes.</p>
-    <b>Donate ADA: addr1q909rs5txh43r7suvyr2kpgs9cjphqjexm00xpnnq2967dsvu27m4c5lj5lc5amqfq8wjaepvg93wvhg4wqhm5es5uqqtqpa6a</b>
+    <p>Clone my public <a href="https://github.com/nftgater/cip8apptest2">github repo</a>, check the README for notes.</p>
+    <p>This repo properly applies the front-end portion of message signing for user authentication per this guide.</p>
+    <p>Donate ADA? <b style="color:white;">addr1q909rs5txh43r7suvyr2kpgs9cjphqjexm00xpnnq2967dsvu27m4c5lj5lc5amqfq8wjaepvg93wvhg4wqhm5es5uqqtqpa6a</b></p>
   </div>
 </template>
 
 <style>
 @media (min-width: 1024px) {
   .about {
-    min-height: 100vh;
-    display: flex;
+    display: block;
     align-items: center;
+    margin: 0 auto;
+    padding: 2rem;
+    line-height: 4em;
   }
 }
 </style>
